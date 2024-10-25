3ff83e98cbb1bf73f3adad3a08bd6bbcf9054355
gogonikkei <hahquqqhoaoz7wa@gmail.com>
2024-10-25 16:30:17 +0900

    protoカリキュ③投稿機能
    適切なビューファイルを設置しましょう
    まで

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,13 +1,36 @@
 class PrototypesController < ApplicationController
   def index
-    @prototypes = Prototype.all
+    @prototypes = Prototype.includes(:user).all
+    # includesを追加してN+1問題を回避
+  end
+
+  def show
+  end
+
+  # @prototypes = Prototype.includes(:user).all:
+  # Prototypeモデルのすべてのレコードを取得し、@prototypesに代入
+  # このとき、関連するUserモデルも一緒にロードする
+  def new
     @prototype = Prototype.new
   end
 
   def create
-    Prototype.create(prototype_params)
-    redirect_to '/'
+    @prototype = Prototype.new(prototype_params)
+    if @prototype.save
+      redirect_to root_path
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
+  # prototype_paramsメソッドを使って受け取ったフォームデータから
+  # 新しいPrototypeオブジェクトを生成し、保存を試みます。保存に
+  # 成功すれば、ルートパスにリダイレクトし、失敗すれば新規作成フォームを再表示します。
+
+  # renderメソッドは、Railsビューにおいて特定のテンプレートや部分
+  # テンプレート（パーシャル）を表示するために使用されます。特に、
+  # partial: "partial_name", collection: collectionの形式
+  # で使われると、指定された部分テンプレートをコレクションの各要素
+  # に対して繰り返しレンダリングします。
 
   private
 
@@ -15,10 +38,3 @@ class PrototypesController < ApplicationController
     params.require(:prototype).permit(:title, :catch_copy, :concept, :image).merge(user_id: current_user.id)
   end
 end
-
-# merge(user_id: current_user.id)を追加しているのは
-# prototypeテーブルがuserテーブルと関連付けられており、user_idが必須
-# （null: false）だから。
-
-# current_user.idは、Deviseで提供される現在ログインしている
-# ユーザーのIDを取得するメソッド。
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'prototypes#index'  # ルートパスをprototypesのindexアクションに設定
-  resources :tweets, only: [:index, :new, :create]
+  resources :prototypes
 end
+
+# resourcesの後は複数形に
