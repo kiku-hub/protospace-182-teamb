e7592a77a4b526fbaff0e4a9168d3451f3b7bbda
kiku-hub <tacyskani@gmail.com>
2024-10-26 13:54:41 +0900

    コントローラーの作成、アクションとルーティングを設定

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   resources :prototypes, only: [:index, :show, :new, :create, :edit, :update, :destroy]  # prototypesコントローラーの全アクションを定義
 
   resources :users, only: [:show] do
-    resources :prototypes, only: [:index]  # 特定のユーザーに関連するプロトタイプを取得
+  resources :prototypes, only: [:index]  # 特定のユーザーに関連するプロトタイプを取得
+  resources :comments, only: [:create]  # prototypesに対するcommentsのcreateアクションのルーティングを追加
   end
 end
